aa39eed5bfe9d13b29c343469ef0155909fe2bea
Nikhil <nravi6@asu.edu>
2022-11-05 14:36:31 -0400

    Updated Work class to a dataclass

--- src/ao3scraper/Work.py
+++ src/ao3scraper/Work.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 from bs4 import BeautifulSoup
@@ -52,17 +53,47 @@ CLASS_PROPERTIES = (
         "language",
         "kudos_by",
         "bookmarks_by",
+        "comments_by",
     ]
     + LIST_TAGS
     + STATS
 )
 
 
+@dataclass
 class Work:
-    def __init__(self, work_id: str, extras: bool = True):
-        self.work_id = work_id
-        self.extras = extras
+    work_id: str | int
+    extras: bool = field(repr=False)
 
+    url: str = field(init=False)
+    title: str = field(init=False)
+    summary: str = field(init=False)
+    authors: list[str] | str = field(init=False)
+    language: str = field(init=False)
+    rating: list[str] | str = field(init=False)
+    warning: list[str] | str = field(init=False)
+    category: list[str] | str = field(init=False)
+    fandom: list[str] | str = field(init=False)
+    relationship: list[str] | str = field(init=False)
+    character: list[str] | str = field(init=False)
+    freeform: list[str] | str = field(init=False)
+    published: datetime = field(init=False)
+    status: Optional[datetime] = field(init=False)
+    complete: bool = field(init=False)
+    words: int = field(init=False)
+    chapters: str = field(init=False)
+    chapters_published: int = field(init=False)
+    chapters_expected: int | str = field(init=False)
+    comments: int = field(init=False)
+    kudos: int = field(init=False)
+    bookmarks: int = field(init=False)
+    hits: int = field(init=False)
+
+    kudos_by: Optional[list] = field(init=False, repr=False)
+    bookmarks_by: Optional[list] = field(init=False, repr=False)
+    comments_by: Optional[list] = field(init=False, repr=False)
+
+    def __post_init__(self):
         self.url = BASE_URL.format(work_id=self.work_id)
 
         self._scrape_page()
@@ -70,9 +101,6 @@ class Work:
 
     def __eq__(self, __o: object) -> bool:
         return __o.work_id == self.work_id
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}(work_id={self.work_id})"
 
     def _scrape_page(self):
         print("Scraping Main page...", end="")
@@ -94,6 +122,10 @@ class Work:
             self._scrape_comments()
             self._scrape_bookmarks()
             self._scrape_kudos()
+        else:
+            self.kudos_by = None
+            self.bookmarks_by = None
+            self.comments_by = None
 
     def _parse_title(self):
         print("Parsing title...", end="")
@@ -116,11 +148,23 @@ class Work:
     def _parse_header(self):
         print("Parsing headers...", end="")
         self.metadata = self.soup.find("dl", class_="work meta group")
-        self.tags = {
-            tag: [subtags.contents[0].contents[0] for subtags in self.metadata.find("dd", class_=tag).find_all("li")]
-            for tag in LIST_TAGS
-        }
+        self.tags = {}
+        for tag in LIST_TAGS:
+            subtags = self.metadata.find("dd", class_=tag)
+            if subtags:
+                self.tags[tag] = [subtag.contents[0].contents[0] for subtag in subtags.find_all("li")]
+            else:
+                self.tags[tag] = []
+
         self.tags["language"] = self.metadata.find("dd", class_="language").contents[0].strip()
+        self.rating = self.tags["rating"]
+        self.warning = self.tags["warning"]
+        self.category = self.tags["category"]
+        self.fandom = self.tags["fandom"]
+        self.relationship = self.tags["relationship"]
+        self.character = self.tags["character"]
+        self.freeform = self.tags["freeform"]
+        self.language = self.tags["language"]
         print("Done")
 
     def _parse_stats(self):
@@ -131,28 +175,48 @@ class Work:
         for stat in ["published", "status"]:
             if stat in self.stats:
                 self.stats[stat] = datetime.strptime(self.stats[stat].text, "%Y-%m-%d")
-        self.stats["complete"] = (
-            True
-            if self.metadata.find("dd", class_="stats").find_all("dt", class_="status")[0].contents[0].split(":")[0]
-            == "Completed"
-            else False
-        )
+        if "status" in self.stats:
+            if (
+                self.metadata.find("dd", class_="stats").find_all("dt", class_="status")[0].contents[0].split(":")[0]
+                == "Completed"
+            ):
+                self.stats["complete"] = True
+            else:
+                self.stats["complete"] = False
+        else:
+            self.stats["status"] = self.stats["published"]
+            self.stats["complete"] = True
         self.stats["chapters_published"], self.stats["chapters_expected"] = self.stats["chapters"].split("/")
         for stat in ["words", "comments", "kudos", "hits", "chapters_published"]:
             if stat in self.stats:
                 self.stats[stat] = int(self.stats[stat])
+            else:
+                self.stats[stat] = 0
         if "bookmarks" in self.stats:
             self.stats["bookmarks"] = int(self.stats["bookmarks"].contents[0])
+        else:
+            self.stats["bookmarks"] = 0
         self.stats["chapters_expected"] = (
             self.stats["chapters_expected"]
             if self.stats["chapters_expected"] == "?"
             else int(self.stats["chapters_expected"])
         )
+        self.published = self.stats["published"]
+        self.status = self.stats["status"]
+        self.complete = self.stats["complete"]
+        self.words = self.stats["words"]
+        self.chapters = self.stats["chapters"]
+        self.chapters_published = self.stats["chapters_published"]
+        self.chapters_expected = self.stats["chapters_expected"]
+        self.comments = self.stats["comments"]
+        self.kudos = self.stats["kudos"]
+        self.bookmarks = self.stats["bookmarks"]
+        self.hits = self.stats["hits"]
         print("Done")
 
     def _scrape_comments(self):
         # //TODO: Get list of comments left
-        pass
+        self.comments_by = []
 
     def _scrape_kudos(self, query_delay: int = 5):
         print("Scraping Kudos...|", end="")
