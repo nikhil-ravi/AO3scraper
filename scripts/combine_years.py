1db4198a10a5ba921eb5f28c8170e540ed3f8422
Nikhil <nravi6@asu.edu>
2022-11-06 13:17:15 -0500

    Added script to combine all the years of work ids scraped.

---
+++ scripts/combine_years.py
@@ -0,0 +1,20 @@
+from glob import glob
+import pandas as pd
+
+
+def main():
+    years = glob("./data/HP_*.csv")
+    pd.concat(
+        [
+            pd.read_csv(year, header=None, compression="gzip")
+            .rename(columns={0: "id"})
+            .assign(year=int(year.split("_")[1].split(".")[0]))
+            for year in years
+        ],
+        ignore_index=True,
+        axis=0,
+    ).to_csv("./data/HP.csv.gzip", index=False, compression="gzip")
+
+
+if __name__ == "__main":
+    main()
